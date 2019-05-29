f1b3f22602c518fa3ff5ccdf16e9e0563106d1ae
zhengbigbig <780357902@qq.com>
2019-05-29 11:00:09 +0800

    private final setter getter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Human def= new Human();
-        System.out.println(def.name);
-        System.out.println(def.height);
+        System.out.println(def.getName());
+        System.out.println(def.getHeight());
 
         Human bob = new Human("bob",120);
-        System.out.println(bob.name);
-        System.out.println(bob.height);
+        System.out.println(bob.getName());
+        System.out.println(bob.getHeight());
 
         bob.rename("jack");
         Human jack = bob; // bob -> Human("jack",120), jack -> Human("jack",120)
@@ -20,5 +20,7 @@ public class Main {
 
         int newHeight = jack.workout();
         System.out.println(newHeight);
+
+        System.out.println(Human.getNumber());
     }
 }
