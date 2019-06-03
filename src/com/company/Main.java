bc8d5dc74a7f0a8d0b6ee7e9c1fa2e0fa2b83332
zhengbigbig <780357902@qq.com>
2019-06-03 09:47:53 +0800

    i/o

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // 标准输出流
+        PrintStream val = System.out;
+        // 标准输入流
+        InputStream val2 = System.in;
+        InputStreamReader stdRender = new InputStreamReader(System.in); //字节流转字符流
+        char[] buffer = new char[20];
+        int length = 0;
+        try {
+            length = stdRender.read(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(length);
+
     }
 }
